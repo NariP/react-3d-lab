32e6d8b6f49953655a8fc6236fb510f6e26f5dc4
박나리 <tto9208@naver.com>
2025-01-13 16:49:26 +0900

    feat: react-three eslint플러그인 설정
    - react/no-unknown-property eslint 에러를 해결하기 위해 설치, but 이 plugin 에서 해당 문제를 해결해주지는 않음

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: '@nr2p/eslint-config',
+  extends: ['@nr2p/eslint-config', 'plugin:@react-three/recommended'],
+  plugins: [
+    '@react-three'
+  ],
+  rules: {
+    "react/no-unknown-property": "off"
+  }
 };
